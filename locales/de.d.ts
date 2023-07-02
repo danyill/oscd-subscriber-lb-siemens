1b0a26e6e190f76991250b1eab860aa5ebb45130
danyill <danyill@users.noreply.github.com>
2023-07-02 20:53:50 +0000

    Deploying to gh-pages from @ danyill/oscd-subscriber-lb-siemens@e068cf03aba831a642d5955ec7fe7c617df7c36a 🚀

---
+++ locales/de.d.ts
@@ -0,0 +1,9 @@
+export declare const templates: {
+    s074ad46563eaf3db: string;
+    s21340f1764775ba9: string;
+    s2813accd17e88786: string;
+    s31b126d31226accd: string;
+    s55e247235f637dbd: string;
+    s5e8250fb85d64c23: string;
+    s965c503c3e42fdfe: string;
+};
---
+++ oscd-subscriber-lb-siemens.d.ts
@@ -0,0 +1,27 @@
+import { LitElement, TemplateResult } from 'lit';
+import '@material/mwc-button';
+import '@material/mwc-dialog';
+import '@material/mwc-formfield';
+import '@material/mwc-switch';
+import type { Dialog } from '@material/mwc-dialog';
+import type { Switch } from '@material/mwc-switch';
+import { EditEvent, Update } from '@openscd/open-scd-core';
+/**
+ * Check if the ExtRef is already subscribed to a FCDA Element.
+ *
+ * @param extRef - The Ext Ref Element to check.
+ */
+export declare function isSubscribedEv(update: Update): boolean;
+export default class SubscriberLaterBindingSiemens extends LitElement {
+    /** The document being edited as provided to plugins by [[`OpenSCD`]]. */
+    doc: XMLDocument;
+    docName: string;
+    dialogUI?: Dialog;
+    enabledUI?: Switch;
+    enabled: boolean;
+    constructor();
+    run(): Promise<void>;
+    protected processSiemensExtRef(extRef: Element): void;
+    protected createAdditionalExtRefs(event: EditEvent): void;
+    render(): TemplateResult;
+}

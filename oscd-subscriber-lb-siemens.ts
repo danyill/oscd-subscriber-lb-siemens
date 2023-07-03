066fa419ddc77aba5745dc665bbccefd4c91f99c
danyill <danyill@users.noreply.github.com>
2023-07-03 14:54:12 +1200

    Update oscd-subscriber-lb-siemens.ts
    Add workaround for old open-scd-core version

--- oscd-subscriber-lb-siemens.ts
+++ oscd-subscriber-lb-siemens.ts
@@ -284,6 +284,10 @@ export default class SubscriberLaterBindingSiemens extends LitElement {
     });
   }
 
+  protected firstUpdated(): void {
+    this.parentElement?.setAttribute('style', 'opacity: 1');
+  }
+  
   render(): TemplateResult {
     return html`<mwc-dialog
       id="dialog"
